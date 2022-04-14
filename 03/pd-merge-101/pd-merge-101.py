fbd30a23de31d29b10a21e8ae5ac96506aa11868
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-04-14 14:19:12 -0500

    📝 Create notebook

---
+++ 03/pd-merge-101/pd-merge-101.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # `pd-merge-101`
+#
+# <div align="center">
+#     <img src="images/pd-merge-101.png" style="width: 65%; border-radius: 10px">
+# </div>
+#
+# 🖼 Image source: screenshot of resource below on April 14, 2022
+#
+# ---
+#
+# [📝 StackOverflow answer](https://stackoverflow.com/questions/53645882/pandas-merging-101)
+#
